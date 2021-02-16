a236131c8f1f4b876604fc34f8e7ac1e6e1be0c9
sareena <sareena.naser@ibm.com>
2021-02-16 15:33:23 +0000

    new changes

---
+++ src/Screens/CreateScreen.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { StyleSheet} from 'react-native'
+import BlogPostForm from '../Components/BlogPostForm';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = ({navigation}) => {
+
+    const {addBlogPost} = useContext(Context)
+    return <BlogPostForm onSubmit={(title, content)=> {
+        addBlogPost(title, content, () => {
+            navigation.navigate('Index')
+        })
+    }}/>
+    
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default CreateScreen
