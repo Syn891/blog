28913663b0e6056b44c6e71f7fbc4629af4ac055
Sareena <sareena.naser@ibm.com>
2021-02-16 15:26:39 +0000

    server api added

---
+++ src/api/jsonserver.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default axios.create({
+
+    baseURL: 'http://8ab7d881af9a.ngrok.io'
+    
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,11 @@
 import createDataContext from './createDataContext'
+import jsonserver from '../api/jsonserver'
 
 const blogReducer = (state, action) => {
 
     switch (action.type) {
+        case 'get_blogposts': 
+            return action.payload
         case 'add_blogpost':
             return [...state, {id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content}]
         case 'delete_blogpost':
@@ -10,6 +13,14 @@ const blogReducer = (state, action) => {
         default:
             return state;
 
+    }
+}
+
+const getBlogPosts = dispatch => {
+    return async () => {
+        const response = await jsonserver.get('/blogposts');
+
+        dispatch ({ type: 'get_blogposts', payload: response.data})
     }
 }
 
@@ -28,4 +39,4 @@ const deleteBlogPost = (dispatch) => {
  
 
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{title: "TEST POST", content: "TEST CONTENT", id: 1}])
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, getBlogPosts }, [])
